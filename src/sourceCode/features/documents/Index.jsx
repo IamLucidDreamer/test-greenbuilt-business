ac1db821b3cdd2a9da57f0c3675518a7550e8a75
Manas Shukla <shuklamanasofficial@gmail.com>
2022-03-24 23:58:25 +0530

    Document Upload Added

--- src/sourceCode/features/documents/Index.jsx
+++ src/sourceCode/features/documents/Index.jsx
@@ -1,17 +1,51 @@
+import React, { useState } from "react";
 import { Button } from "antd";
-import React from "react";
+import axios from "../../appConfig/httpHelper";
+import { toast } from "react-toastify";
 
 export const Documents = () => {
-  const uplaodDocument = (docName) => {};
+  const token = JSON.parse(localStorage.getItem("jwt"));
+
+  const [fileName, setFileName] = useState("");
+  const [file, setFile] = useState("");
+
+  const uplaodDocument = () => {
+    const formData = new FormData();
+    formData.append("title", fileName);
+    formData.append("file", file);
+    axios
+      .post("/document/upload/1", formData, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => toast.success(res.data.message))
+      .catch((err) => console.log(err));
+  };
   return (
-    <div className="mt-4 flex gap-4">
-      <Button
-        type="primary"
-        onClick={() => uplaodDocument("Power Purchase Agreement")}
-      >
-        Power Purchase Agreement
-      </Button>
-      <Button type="primary">Energy Wheeling Agreement</Button>
+    <div className="mt-4 ">
+      <div className="flex gap-4">
+        <Button
+          type="primary"
+          onClick={() => setFileName("Power Purchase Agreement")}
+        >
+          Power Purchase Agreement
+        </Button>
+        <Button
+          type="primary"
+          onClick={() => setFileName("Energy Wheeling Agreement")}
+        >
+          Energy Wheeling Agreement
+        </Button>
+      </div>
+      {fileName !== "" ? (
+        <>
+          <input
+            type="file"
+            className="p-4 border-4 rounded-lg border-purple-1 mt-4"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <Button onClick={() => uplaodDocument()}>Upload</Button>
+        </>
+      ) : null}
     </div>
   );
 };
